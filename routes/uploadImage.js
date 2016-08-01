4d495d96d4b7e6afdc541c421fe348f520a9c958
Tsubasa Hayashi <yoku.rin.99@gmail.com>
2016-08-01 17:34:35 +0900

    fix

--- routes/uploadImage.js
+++ routes/uploadImage.js
@@ -138,7 +138,7 @@ router.post('/', multipartMiddleware, function(req, res, _next) {
 				console.log("python results: \n", stdout);
 				// json 格納
 				//newResults = stdout;
-				newResults = stdout;
+				newResults = JSON.parse(stdout);
 				if(err){
 					console.log("err:\n", err);
 					return res.status(500).send({
